d3bb831e08304ed551fa0d32a47cc5e54a28f809
Vio <vioice7@yahoo.com>
2022-07-19 12:16:40 +0300

    mitest

--- mink01.php
+++ mink01.php
@@ -15,44 +15,80 @@ $driver = new Selenium2Driver();
 $session = new Session($driver);
 
 $session->start();
-$session->visit('http://automationpractice.com/index.php');
+//$session->visit('http://automationpractice.com/index.php');
+$session->visit('http://automationpractice.com/index.php?id_product=1&controller=product');
 
+// Get the current url
+echo "Current URL: ". $session->getCurrentUrl() . "\n";
 
-
-//echo "Status code: ". $session->getStatusCode() . "\n";
-//echo "Current URL: ". $session->getCurrentUrl() . "\n";
-
+// Get the current page
 $page = $session->getPage();
 
+// Check if there is the add to cart button element on the current page
+$el = $page->find('css', '#add_to_cart');
 
-$selectorsHandler = $session->getSelectorsHandler();
-$linkEl = $page->find(
-    'named',
-    array(
-        'link',
-        $selectorsHandler->xpathLiteral('Add to cart')
-    )
-);
-echo "The link href is: ". $linkEl->getAttribute('href') . "\n";
+if (null === $el) {
+    echo "The element is not found!\n";
+} else {
+    echo "Element was found!\n"; 
+}
 
-$linkEl->click();
+// Clik that button
+$el->click();
 
-echo "Page URL after click: ". $session->getCurrentUrl() . "\n";
+// Wait for loading
+sleep(5);
 
-$text = $page->find('css', 'h2 .ajax_cart_product_txt_s');
+// Check if there is a product in the cart
+$el = $page->find('css', '.layer_cart_product');
 
-// $test = print_r($text, TRUE);
+if (null === $el) {
+    echo "The element is not found!\n";
+} else {
+    echo "Element was found!\n"; 
+}
 
-// file_put_contents('test.txt', $test);
-
-echo "Page has text: " . $text->getText();
-
+// Get the text for that product
+echo $el->getText();
 echo "\n";
+
+
+// Check if there is a button to proceed
+$el = $page->find('css', '.button-medium');
+
+if (null === $el) {
+    echo "The element is not found!\n";
+} else {
+    echo "Element was found!\n"; 
+}
+
+$el->click();
+
+sleep(3);
+
+$number_pixels = 300;
+$session->executeScript("window.scrollBy(0, ".$number_pixels.");");
+
+sleep(3);
+
+
+// Check if there is a button to proceed
+$el = $page->find('css', '.standard-checkout');
+
+if (null === $el) {
+    echo "The element is not found!\n";
+} else {
+    echo "Element was found!\n"; 
+    $el->click();
+}
+
+
 
 
 
 $session->stop();
 
+echo "\n\n\n";
 
 
 
